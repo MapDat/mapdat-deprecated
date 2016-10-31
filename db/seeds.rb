9036d7eb7c8d97ad5faf537bb6b43e7f24df0e9e
Conor Landry <clandry94@ufl.edu>
2016-10-31 13:05:59 -0400

    More seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,46 @@ require 'json'
 uri = URI.parse('http://campusmap.ufl.edu/library/cmapjson/geo_buildings.json')
 response = Net::HTTP.get(uri)
 data = JSON.parse(response)
+geo_buildings = []
 
+count = 0
 data['features'].each do |feature|
-    name = feature['properties']['NAME']
-    puts "Name: #{name}"
+  props = feature['properties']  
+  jtype = props['JTYPE']
+  bldg_num = props['BLDG']
+  abbrev = props['ABBREV']
+  name = props['NAME']
+  address = props['ADDRESS1']
+  city = props['CITY']
+  state = props['STATE']
+  zip = props['ZIP']
+  description = props['DESCRIPTION']
+  url = props['url']
+  photo = props['PHOTO']
+  
+
+  shape =  feature['geometry']['type']
+
+  points = []
+  feature['geometry']['coordinates'].each do |point| 
+    points << [point[0], point[1]]
+    count += 1
+  end
+
+
+  prop_hash = { jtype: jtype, bldg_num: bldg_num, abbrev: abbrev,
+                address: address, city: city, state: state,
+                zip: zip, desc: description, url: url,
+                remote_photo_path: photo, geo_points: points }
+  count += prop_hash.count
+  puts prop_hash 
+  geo_buildings << prop_hash
 end
+
+puts count
+
+
+
+
+
+
