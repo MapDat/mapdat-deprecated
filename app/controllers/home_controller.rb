23a50574ae2398dda779b53c9110ae09dc6f189f
Conor Landry <clandry94@ufl.edu>
2016-11-17 02:07:23 -0500

    Working react

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,17 @@ class HomeController < ApplicationController
   def index
     @connection = ActiveRecord::Base.connection
 
-    @buildings = @connection.exec_query("SELECT * FROM building b, map_object m
-                                        WHERE b.object_id = m.id")
+    @buildings = @connection.exec_query("SELECT * FROM building b, map_object m, geo_point g
+                                         WHERE b.object_id = m.id AND g.object_id = m.id")
+
+
 
     buildings = []
     @buildings.rows.each do |building|
       buildings << [building[7], building[8], building[9], '', 0, 0, 0, 0]
     end
     puts buildings
-    render component: 'Building', props: { buildings: buildings}, tag: 'span', class: 'home'
+    render component: 'Building', props: { buildings: @buildings[0..100]}, tag: 'span', class: 'home'
   end
 
 
