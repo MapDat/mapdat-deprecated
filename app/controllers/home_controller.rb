9913a0bb2c635be05a2ebb9bb07b977188dde1c5
Conor Landry <clandry94@ufl.edu>
2016-12-03 18:01:46 -0500

    THE LINES WORK

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,8 +7,20 @@ class HomeController < ApplicationController
     @objects = @connection.exec_query("SELECT m.id, g.longitude, g.latitude
                                        FROM map_object m, geo_point g
                                        WHERE g.object_id = m.id
-                                      ")
-    @objects.rows
+                                      ").rows
+
+
+    @polylines = []
+    @objects.each do |object|
+      object_id = object[0]
+      if @polylines[object_id].nil?
+        @polylines[object_id] = Hash.new
+        @polylines[object_id][:latlngs] = []
+      end
+      @polylines[object_id][:latlngs] << [object[1], object[2]]
+    end
+
+    @polylines = @polylines.reject { |e| e.to_s.empty? }
   end
 
 end
